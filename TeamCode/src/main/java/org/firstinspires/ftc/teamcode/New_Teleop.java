f43b740a16e2ba7c1c14b23a24456ee1afab8534
reaganmitton <142063056+reaganmitton@users.noreply.github.com>
2025-12-05 20:36:10 -0500

    Update New_Teleop.java
    changed teleop for scrim

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New_Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New_Teleop.java
@@ -1,68 +1,73 @@
+// package and imports
+
+// a package defines where our code is stored
 package org.firstinspires.ftc.teamcode;
 
+// imports bring in LinearOpMode, motors, servos, etc.
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 
-@TeleOp(name = "New Teleop v5")
-public class New_Teleop extends LinearOpMode {
+@TeleOp(name = "ray tele v8")
+public class New_Teleop extends OpMode {
 
+    // declaring hardware
     private DcMotor frontLeft;
-    private DcMotor frontRight;
     private DcMotor backLeft;
+    private DcMotor frontRight;
     private DcMotor backRight;
-    private DcMotor rollerMotor;
+    private DcMotor backRollerMotor;
+    private DcMotor frontRollerMotor;
+
 
     @Override
-    public void runOpMode() {
-
+    public void init() {
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
-        rollerMotor = hardwareMap.get(DcMotor.class, "rollerMotor");
+        backRollerMotor = hardwareMap.get(DcMotor.class, "rollerMotor");
+        frontRollerMotor = hardwareMap.get(DcMotor.class, "leftEncoder");
+        
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+    }
 
-        //change directions?
-        frontLeft.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setDirection(DcMotor.Direction.REVERSE);
-        rollerMotor.setDirection(DcMotor.Direction.REVERSE);
+    @Override
+    public void loop() {
+        double drive = +gamepad1.left_stick_y;
+        double turn = gamepad1.left_stick_x;
 
+        drive = Range.clip(drive, -1, 1);
+        turn = Range.clip(turn, -1, 1);
 
-        waitForStart();
+        frontLeft.setPower(drive + turn);
+        backLeft.setPower(drive + turn);
+        frontRight.setPower(drive - turn);
+        backRight.setPower(drive - turn);
 
-        if (opModeIsActive()) {
+        //frontLeft.setPower(Range.clip(gamepad1.left_stick_x, -1, 1));
+        //frontRight.setPower(Range.clip(gamepad1.right_stick_y, -1, 1));
+        //backLeft.setPower(Range.clip(gamepad1.left_stick_y, -1, 1));
+        //backRight.setPower(Range.clip(gamepad1.right_stick_y, -1, 1));
 
-            while (opModeIsActive()) {
+        if (gamepad1.right_trigger > 0){
+            backRollerMotor.setPower(-0.75);
+            frontRollerMotor.setPower(-0.75);
+        } else {
+            backRollerMotor.setPower(0);
+            frontRollerMotor.setPower(0);
+        }
 
-                double y = -gamepad1.left_stick_y;
-                double x = gamepad1.right_stick_x * 0.7;
-                double r = gamepad1.left_stick_x;
-                double m = -(gamepad2.right_stick_y * 0.5);
-                double b = gamepad2.left_stick_y * 0.65;
-                boolean bumperR = gamepad1.right_bumper;
-                float triggerR = gamepad1.right_trigger;
-                boolean bumperL = gamepad1.left_bumper;
-
-                double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(r), 1);
-
-                frontLeft.setPower((y + x + r) / denominator);
-                backLeft.setPower((y - x + r) / denominator);
-                frontRight.setPower((y - x - r) / denominator);
-                backRight.setPower((y + x - r) / denominator);
-
-                if (bumperR) {
-                    rollerMotor.setPower(1);
-                } else{
-                    rollerMotor.setPower(0);
-                }
-
-                if(bumperL){
-                    rollerMotor.setPower(-1);
-                } else {
-                    rollerMotor.setPower(0);
-                }
-            }
+        if (gamepad1.right_bumper){
+            backRollerMotor.setPower(0.75);
+            frontRollerMotor.setPower(0.75);
+        } else {
+            backRollerMotor.setPower(0);
+            frontRollerMotor.setPower(0);
         }
     }
 }
