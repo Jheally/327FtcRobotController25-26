a345408566d07edde37cba3fe54624f9383eac03
Reagan <reaganmitton@gmail.com>
2026-02-18 20:42:26 -0500

    my chopped code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcamExample.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.mechanisms.AprilTagWebcam;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+public class AprilTagWebcamExample extends OpMode {
+    AprilTagWebcam aprilTagWebcam = new AprilTagWebcam();
+    @Override
+    public void init(){
+        aprilTagWebcam.init(hardwareMap, telemetry);
+    }
+
+    @Override
+    public void loop() {
+        aprilTagWebcam.update();
+        AprilTagDetection id20 = aprilTagWebcam.getTagBySpecificID(20);
+        aprilTagWebcam.displayDetectionTelemetry(id20);
+    }
+}
